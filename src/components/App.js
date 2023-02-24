dfe2ba09a5b452522a3445e3ea004171dce2a527
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-02-24 22:15:03 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -35,14 +35,23 @@ class App extends Component {
     { name: 'Mount Abu', country: 'India' },
     { name: 'Tirupati', country: 'India' },
     ]
+    this.valid = this.valid.bind(this);
   }
-
+valid() {
+    let a = [];
+    let j = 1;
+    for (let i = 0; i < this.cityList.length; i++) {
+      if (this.cityList[i].country === "India") {
+        a.push(<li key={"location" + j++}>{this.cityList[i].name}</li>);
+        j++;
+      }
+    }
+    return a;
+  }
   render() {
-    return (
-      <div id="main">
-        {/* Do not remove the main div */}
-      </div>
-    )
+
+      return <ol>{this.valid()}</ol>;
+    
   }
 }
 
