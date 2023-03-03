f19ba5892ab5087ff05739615ccc1cdbd12db4b4
Prakash842277 <121437397+Prakash842277@users.noreply.github.com>
2023-03-03 14:12:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// function generateCodeFromObject(obj){
+//     //return a code generated string
+//    }
    
-module.exports=generateCodeFromObject;
+// module.exports=generateCodeFromObject;
 
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+// // ReactDOM.render(<App />, document.getElementById("root"));
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
